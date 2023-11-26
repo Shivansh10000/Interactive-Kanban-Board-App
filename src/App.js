35e60a1677927d9fefd6fdb41e538a2b9979162f
Shivansh10000 <shivanshj157@gmail.com>
2023-11-26 17:37:26 +0530

    integrated users and tickets

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import { useState, useEffect } from 'react';
 import Card from './Card.jsx';
-import Selector from './Selector.jsx'
-import './styles.css'
+import Selector from './Selector.jsx';
+import './styles.css';
 
 const App = () => {
   const API_URL = 'https://api.quicksell.co/v1/internal/frontend-assignment';
 
   const [tickets, setTickets] = useState([]);
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
   const [groupBy, setGroupBy] = useState('status');
   const [groupedTickets, setGroupedTickets] = useState({});
-  const [sortBy, setSortBy] = useState('title')
+  const [sortBy, setSortBy] = useState('title');
+
+  const priorityMap = {
+    4: 'Urgent',
+    3: 'High',
+    2: 'Medium',
+    1: 'Low',
+    0: 'No priority',
+  };
+
+  const getRequiredKey = (key) => {
+    const priority = priorityMap[parseInt(key)];
+    const userName = userIdToNameMap[key];
+
+    if (priority) {
+      return priority;
+    } else if (userName) {
+      return userName;
+    } else {
+      return key;
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,7 +40,7 @@ const App = () => {
         const data = await response.json();
         console.log(data);
         setTickets(data.tickets);
-        setUsers(data.users)
+        setUsers(data.users);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -36,9 +57,15 @@ const App = () => {
     }
   }, [tickets, groupBy, sortBy]);
 
+  const userIdToNameMap = {};
+
+  users.forEach((user) => {
+    userIdToNameMap[user.id] = user.name;
+  });
+
   const groupByKey = (tickets, key, sortKey) => {
     const grouped = {};
-  
+
     tickets.forEach((ticket) => {
       const keyValue = ticket[key];
       if (!grouped[keyValue]) {
@@ -46,11 +73,11 @@ const App = () => {
       }
       grouped[keyValue].push(ticket);
     });
-  
+
     // Sort each group's array based on sortKey
     for (const groupKey in grouped) {
       const groupTickets = grouped[groupKey];
-  
+
       let sortedGroupTickets;
       if (sortKey === 'title') {
         sortedGroupTickets = groupTickets.sort((a, b) => {
@@ -61,16 +88,15 @@ const App = () => {
       } else if (sortKey === 'priority') {
         sortedGroupTickets = groupTickets.sort((a, b) => b[sortKey] - a[sortKey]);
       }
-  
+
       grouped[groupKey] = sortedGroupTickets;
     }
-  
+
     setGroupedTickets(grouped);
   };
-  
+
   return (
     <div className="App">
-
       <div className='navbar'>
         <div className="navbar-left">
           <Selector tickets={tickets} groupByKey={groupByKey} groupBy={groupBy} sortBy={sortBy} groupedTickets={groupedTickets} />
@@ -84,21 +110,19 @@ const App = () => {
         <div className='title'>Kanban Board Application</div>
 
         <div className='temp-top'>
-            {Object.keys(groupedTickets).map((groupKey) => (
-              <div key={groupKey} className='column'>
-                <h2 className='column-name'>
-                  <div>{groupKey} </div>
-                  <div>{groupedTickets[groupKey].length}</div> 
-                </h2>
-                {groupedTickets[groupKey].map((ticket) => (
-                  <Card key={ticket.id} ticket={ticket} users={users} className="Card" />
-                ))}
-              </div>
-            ))}
-          
+          {Object.keys(groupedTickets).map((groupKey) => (
+            <div key={groupKey} className='column'>
+              <h2 className='column-name'>
+                <div>{getRequiredKey(groupKey)} </div>
+                <div>{groupedTickets[groupKey].length}</div> 
+              </h2>
+              {groupedTickets[groupKey].map((ticket) => (
+                <Card key={ticket.id} ticket={ticket} users={users} userName={userIdToNameMap[ticket.userId]} className="Card" />
+              ))}
+            </div>
+          ))}
         </div>
       </div>
-
     </div>
   );
 };
