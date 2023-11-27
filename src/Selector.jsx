db1bf8294f33b4ddc8a75fbc85117fe502c0e4f0
Shivansh10000 <shivanshj157@gmail.com>
2023-11-27 13:33:19 +0530

    completed navbar design

--- src/Selector.jsx
+++ src/Selector.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import menuSvg from "./svg/menu.svg";
 
 const Selector = ({ groupByKey, tickets, groupBy, sortBy }) => {
   const [displayDropdownOpen, setDisplayDropdownOpen] = useState(false);
@@ -37,15 +38,26 @@ const Selector = ({ groupByKey, tickets, groupBy, sortBy }) => {
 
   return (
     <div className="dropdown">
-      <div className="display-button" onClick={toggleDisplayDropdown}>
-        Display
+      <div onClick={toggleDisplayDropdown} className="display-button-wrapper">
+        <div className="menu-svg" onClick={toggleDisplayDropdown}>
+          <img src={menuSvg} alt="Menu Icon" />
+        </div>
+        <div className="display-button" onClick={toggleDisplayDropdown}>
+          Display
+        </div>
+        <div onClick={toggleDisplayDropdown} className="downward-symbol">
+          &gt;
+        </div>
       </div>
       {displayDropdownOpen && (
         <div className="dropdown-options">
           <div className="dropdown-option-wrapper">
             <div>Grouping</div>
             <div className="dropdown-item" onClick={toggleGroupingDropdown}>
-              {groupCondition}
+              <div className="downward-dropdown">
+                <div className="option-text">{groupCondition}</div>
+                <div className="downward-symbol">&gt;</div>
+              </div>
               {groupingDropdownOpen && (
                 <div className="options">
                   <button onClick={() => handleGroupingClick("status")}>
@@ -64,7 +76,10 @@ const Selector = ({ groupByKey, tickets, groupBy, sortBy }) => {
           <div className="dropdown-option-wrapper">
             <div>Ordering</div>
             <div className="dropdown-item" onClick={toggleOrderingDropdown}>
-              {sortCondition}
+              <div className="downward-dropdown">
+                <div className="option-text">{sortCondition}</div>
+                <div className="downward-symbol">&gt;</div>
+              </div>
               {orderingDropdownOpen && (
                 <div className="options">
                   <button onClick={() => handleOrderingClick("priority")}>
